1d2a6042d44b94cacced73a54eeda61e47ba710b
Flex <zougariakram4@gmail.com>
2025-11-27 16:57:23 +0100

    Initial commit

---
+++ src/test/java/com/akram/seo/SmartEnergyOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akram.seo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEnergyOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
